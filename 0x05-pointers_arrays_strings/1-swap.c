3dae5c9225554259f9667d8303907e7ea6bfd84e
SPWELA <sindisiwemp@gmail.com>
2022-06-13 11:53:50 -0700

    swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,18 +2,14 @@
 
 /**
  * void spw - spwas int
- * @c : input
- * Return (0)
+ * @a : to swop
+ * Return (void)
  */
-void swap_int(int *a, int *b);
-int main(void)
+void swap_int(int *a, int *b)
 {
-	char ch = 'a=98';
-	char ch = 'b=42';
-	for (ch = 'a=98'; ch = 'b=42'; ch++)
-		putchar(ch);
-	return (0);
-	swap _int (&a, &b);
-	printf("a=%d, b=%d\n" , a, b);
-	return (0);
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
