1761d4f7a439e36d9f99380964c0fe9d76b33e01
SPWELA <sindisiwemp@gmail.com>
2022-06-13 11:32:35 -0700

    resét to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - a function to that takes a pointer to an int as parameter and updates the value it points to to 98
- * Return: 98
+ * reset_to_98 - reset to value 98
+ * pointer to reset to 98
+ * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	int 
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * void spw - spwas int
+ * @c : input
+ * Return (0)
+ */
+void swap_int(int *a, int *b);
+int main(void)
+{
+	char ch = 'a=98';
+	char ch = 'b=42';
+	for (ch = 'a=98'; ch = 'b=42'; ch++)
+		putchar(ch);
+	return (0);
+	swap _int (&a, &b);
+	printf("a=%d, b=%d\n" , a, b);
+	return (0);
+}
