39056566162f37bbe5c9cfb76d33209d383d6cc9
SPWELA <sindisiwemp@gmail.com>
2022-06-13 10:06:47 -0700

    start

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - a function to that takes a pointer to an int as parameter and updates the value it points to to 98
+ * Return: 98
+ */
+
+void reset_to_98(int *n);
+{
+	int 
