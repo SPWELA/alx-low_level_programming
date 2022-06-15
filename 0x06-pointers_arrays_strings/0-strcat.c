ac4953582726d16990cdf2d6c83c134d3978e758
SPWELA <sindisiwemp@gmail.com>
2022-06-15 15:50:04 -0700

    2 files saved

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,22 @@
 
 /**
  * *_strcat(char *dest, char *src) -concatenates two strings
- * @n - input data
- * Return (void)
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
  */
-
 char *_strcat(char *dest, char *src)
 {
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
 
