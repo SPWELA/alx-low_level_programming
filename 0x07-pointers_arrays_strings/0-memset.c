bb095999153fa7172173f300ccc5cc95e771a05f
SPWELA <sindisiwemp@gmail.com>
2022-06-20 11:35:15 -0700

    info into files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _memset - fills memory with constant byte
- * @n - unsigned init
- * @s - pointer
- * @b -byte
+ * @n: unsigned init
+ * @s: pointer
+ * @b: byte
  * return (s)
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 	s[i] = b;
-	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,45 +1,33 @@
+#include "main.h"
 
-
-
-
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
+	unsigned int i, n, value, check;
 
-		int i, j;
+	value = 0;
 
-			int c = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
 
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
 
+		if (check == 0)
+			return (value);
+	}
 
-				for (i = 0; s[i] != '\0'; i++)
-
-						{
-
-									if (s[i] != 32)
-
-												{
-
-																for (j = 0; accept[j] != '\0'; j++)
-
-																				{
-
-																									if (s[i] == accept[j])
-
-																															c++;
-
-																												}
-
-																		}
-
-											else
-
-															return (c);
-
-
-
-												}
-
-					return (c);
-
+	return (value);
 }
