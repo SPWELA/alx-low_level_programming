74b23741ba8bfbd8e3f0dec09e9c6c1c10a395a7
SPWELA <sindisiwemp@gmail.com>
2022-07-04 13:58:32 -0700

    create dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
  * struct dog - A dog struct
  * @name: First member
@@ -10,18 +9,13 @@
  *
  * Description: Define a new type struct dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+} dog_t;
 
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
