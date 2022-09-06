b57725039582ea90bcb02fe82fff40ec35284b4b
SPWELA <sindisiwemp@gmail.com>
2022-09-06 14:03:43 -0700

    py code checker

--- 0x18-dynamic_libraries/function.py
+++ 0x18-dynamic_libraries/function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from ctypes import *
-so_file = "/alx-low_level_programming/0x18-dynamic_libraries/100-operations.so"
-my_functions = CDLL(so_file)
+from ctypes
+import random
+so_file = "./100-operations.so"
+my_functions = ctypes.CDLL(so_file)
 
 print(type(my_functions))
 print(my_functions.square(10)
-        print("Done")
+print("Done")
